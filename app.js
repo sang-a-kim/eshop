bd86faef751ce24ef5f73f2cbb4df3552f0ed1fa
김상아 Sang-A Kim <blossom78325@gmail.com>
2023-04-25 17:07:15 +0900

    feat: create an app

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("Hello world")
+import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+import express from "express";
+
+dotenv.config()
+
+const app = express();
+
+const api = process.env.API_URL
+
+app.listen(3000, () => {
+	console.log("Server is listening on http://localhost:3000");
+});
+
+app.get('/', (req, res) => res.send('First'))
