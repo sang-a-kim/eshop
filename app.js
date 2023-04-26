c2f4cad904a78c5131e0da3052c9ea8e85edc286
김상아 Sang-A Kim <blossom78325@gmail.com>
2023-04-26 16:48:15 +0900

    feat: connect database

--- app.js
+++ app.js
@@ -1,14 +1,42 @@
-import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+import * as dotenv from "dotenv";
 import express from "express";
-
-dotenv.config()
+import mongoose from "mongoose";
+import morgan from "morgan";
+dotenv.config();
 
 const app = express();
 
-const api = process.env.API_URL
+// Middleware
+app.use(express.json());
+app.use(morgan("tiny"));
 
-app.listen(3000, () => {
-	console.log("Server is listening on http://localhost:3000");
+const api = process.env.API_URL;
+
+app.get(`${api}/products`, (req, res) => {
+	const product = {
+		id: 1,
+		name: "hair dresser",
+		image: "some_url",
+	};
+
+	res.send(product);
 });
 
-app.get('/', (req, res) => res.send('First'))
+app.post(`${api}/products`, (req, res) => {
+	const newProduct = req.body;
+	res.send(newProduct);
+});
+
+// Connect Datatabse
+mongoose
+	.connect(process.env.DB_URL)
+	.then(() => {
+		console.log("Database conneciton is ready 🚀");
+	})
+	.catch((e) => console.log(e));
+
+
+// Listen the app	
+app.listen(3000, () => {
+	console.log("Server is listening on http://localhost:3000 🚀");
+});
